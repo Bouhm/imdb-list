da02126394957ff0b6c7bd6025f0c1a97c8bd9b1
Bouhm <bbouhm@gmail.com>
2020-02-20 00:57:23 -0800

    fetch genres, fill in genres

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,8 @@ import { useEffect, useState } from "react";
 import { List } from "./List";
 import "./App.css";
 
-interface IData {
-  movies: IMovie[];  
-}
-
 export interface IMovie {
-  genre_ids: number[];
+  genres: string[];
   overview: string;
   popularity: string;
   title: string;
@@ -16,42 +12,81 @@ export interface IMovie {
   release_date: string;
 }
 
+export interface IGenre {
+  [id: number]: string;
+}
+
+const API_URL = "https://api.themoviedb.org/3/";
+
+/**
+ * Fetch genres, map genre id to name in state, cache to local storage
+ */
 export function App(): JSX.Element {
-  const [data, setData] = useState<IData>({movies: []});
-
+  const [genreMap, setGenreMap] = useState<IGenre>({});
   useEffect(function () {
-    const API_URL = "https://api.themoviedb.org/3/";
-
     // Check local storage if cached
-    let moviesData: IMovie[];
-    let storedData = window.localStorage.getItem("data");
+    let genreData: IGenre = {};
+    let storedData = window.localStorage.getItem("genres");
 
     if (!storedData) {
-      fetch(API_URL + `discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc`)
-      .then(function (res: any) { return res.json(); })
-      .then(function (json: any) { 
-        moviesData = json.results.map(function (movie: any) {
-          return {
-            genre_ids: movie.genre_ids,
-            overview: movie.overview,
-            popularity: movie.popularity,
-            title: movie.title,
-            vote_average: movie.vote_average,
-            release_date: movie.release_date,
-          };
+      fetch(
+        API_URL +
+        `genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`,
+      )
+        .then(function (res: any) {
+          return res.json();
+        })
+        .then(function (data: any) {
+          data.genres.forEach(function (genre: any) {
+            genreData[genre.id] = genre.name;
+          });
+
+          setGenreMap(genreData);
+          window.localStorage.setItem("genres", JSON.stringify(genreData));
         });
-        
-        setData({movies: moviesData});
-        window.localStorage.setItem("data", JSON.stringify(moviesData));
-      });
     } else {
-      setData({movies: JSON.parse(storedData)});
+      setGenreMap(JSON.parse(storedData));
     }
-  }, []); 
+  }, []);
+
+  /**
+   * Fetch movies, set state, and cache to local storage
+   */
+  const [movieData, setMovieData] = useState<IMovie[]>([]);
+  useEffect(function () {
+    // Check local storage if cached
+    let movieData: IMovie[];
+    let storedData = window.localStorage.getItem("movieList");
+
+    if (!storedData) {
+      fetch(
+        API_URL +
+        `discover/movie?api_key=${process.env.REACT_APP_API_KEY}&sort_by=popularity.desc`,
+      )
+        .then(function (res: any) {
+          return res.json();
+        })
+        .then(function (data: any) {
+          movieData = data.results.map(function (movie: any) {
+            return {
+              genres: movie.genre_ids.map(function (id: number) { return genreMap[id]; }),
+              overview: movie.overview,
+              popularity: movie.popularity,
+              title: movie.title,
+              vote_average: movie.vote_average,
+              release_date: movie.release_date,
+            };
+          });
+
+          setMovieData(movieData);
+          window.localStorage.setItem("movieList", JSON.stringify(movieData));
+        });
+    } else {
+      setMovieData(JSON.parse(storedData));
+    }
+  }, [genreMap]);
 
   return (
-    <div className="app">
-      {data.movies && <List movies={data.movies} />}
-    </div>
+    <div className="app">{movieData && <List movies={movieData} />}</div>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,33 +1,28 @@
 import * as React from "react";
 import { IMovie } from "./App";
-import * as genres from "../data/genres.json"
 import "./List.css";
 
 export type ListProps = {
   movies: IMovie[];
 };
 
-export function List({movies}: ListProps): JSX.Element {
-  
-  function renderItem({ popularity, title, vote_average, overview, release_date, genre_ids }: IMovie, index: number) {
+export function List({ movies }: ListProps): JSX.Element {
+
+  function renderItem({ popularity, title, vote_average, overview, release_date, genres }: IMovie, index: number) {
     return (
       <div key={index} className="list-item">
         <h3>{title}</h3>
         <p>{overview}</p>
-
-        <span>Score: {vote_average} | </span>
-        <span>Released: {release_date} | </span>
-        <span>
-          Genres: {genre_ids.map(function (genreId, i) { 
-            console.log(genres);
-            const genre = genres.find(function (genre) {
-              return genre.id === genreId;
-            });
-
-            return i === genre_ids.length - 1 ? genre : genre + ", ";
-          })}
-        </span>
-      </div>  
+        <div>
+          <span>Score: {vote_average} | </span>
+          <span>Released: {release_date} | </span>
+          <span>
+            Genres: {genres.map(function (genre, i) {
+              return i === genres.length - 1 ? genre : genre + ", ";
+            })}
+          </span>
+        </div>
+      </div>
     );
   }
 

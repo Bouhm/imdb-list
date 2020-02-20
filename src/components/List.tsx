8e4972f715e6327165162ef0788388b642cf0888
Brian Pak <bbouhm@gmail.com>
2020-02-19 17:02:30 -0800

    basic list items, genres data

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { IMovie } from "./App";
-// import "./List.css";
+import * as genres from "../data/genres.json"
+import "./List.css";
 
 export type ListProps = {
   movies: IMovie[];
@@ -10,15 +11,24 @@ export function List({movies}: ListProps): JSX.Element {
   
   function renderItem({ popularity, title, vote_average, overview, release_date, genre_ids }: IMovie, index: number) {
     return (
-      <div key={index}>
-        {title}
-        {popularity}
-        {vote_average}
-        {overview}
-        {release_date}
-        {genre_ids}
+      <div key={index} className="list-item">
+        <h3>{title}</h3>
+        <p>{overview}</p>
+
+        <span>Score: {vote_average} | </span>
+        <span>Released: {release_date} | </span>
+        <span>
+          Genres: {genre_ids.map(function (genreId, i) { 
+            console.log(genres);
+            const genre = genres.find(function (genre) {
+              return genre.id === genreId;
+            });
+
+            return i === genre_ids.length - 1 ? genre : genre + ", ";
+          })}
+        </span>
       </div>  
-    )
+    );
   }
 
   return (
